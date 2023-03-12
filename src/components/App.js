830abe81bef40d129184862dd81f7fb836ee72d2
shiba_1234 <shibajisahu121@gmail.com>
2023-03-12 17:16:06 +0530

    Project UI is ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import { data } from "../data";
+import MovieCard from "./MovieCard";
+import Navbar from "./Navbar";
+
 function App() {
-  return <div className="App">Shibaji Sahu</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
