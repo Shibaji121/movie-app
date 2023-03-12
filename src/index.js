4a49b73dfa32b22e11821916f212917bbbecccb8
shiba_1234 <shibajisahu121@gmail.com>
2023-03-12 17:53:35 +0530

    dispatching of action done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import movies from "./reducers";
+
+const store = createStore(movies);
+
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Superman" }],
+});
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
