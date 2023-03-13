48786ae8bcdd6d53136d5335f37a25b28644ac11
shiba_1234 <shibajisahu121@gmail.com>
2023-03-13 21:29:42 +0530

    Combine Reducer added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { createStore } from "redux";
-import movies from "./reducers";
+import rootReducer from "./reducers";
 
-const store = createStore(movies);
+const store = createStore(rootReducer);
 
-store.dispatch({
-  type: "ADD_MOVIES",
-  movies: [{ name: "Superman" }],
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "Superman" }],
+// });
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
