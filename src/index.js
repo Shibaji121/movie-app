a22c91471a7d9d9c9b91dd061651f12fbf2f172b
shiba_1234 <shibajisahu121@gmail.com>
2023-03-13 23:28:57 +0530

    Adding middle ware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// Currying function
+// logger(obj)(next)(action)
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       // middleware code
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log("ACTION_TYPE = ", action.type);
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // store.dispatch({
 //   type: "ADD_MOVIES",
