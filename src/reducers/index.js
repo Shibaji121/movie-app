7c0d62c245074bc1359f66b4c5534b5fb4e7cf12
shiba_1234 <shibajisahu121@gmail.com>
2023-03-13 21:34:59 +0530

    comine reducer from redux added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { combineReducers } from "redux";
 import {
   ADD_MOVIES,
   ADD_TO_FAVOURITES,
@@ -54,9 +55,14 @@ const initialRootState = {
   search: initialSearchState,
 };
 
-export default function rootReducer(state = initialRootState, action) {
-  return {
-    movies: movies(state.movies, action),
-    search: search(state.search, action),
-  };
-}
+// export default function rootReducer(state = initialRootState, action) {
+//   return {
+//     movies: movies(state.movies, action),
+//     search: search(state.search, action),
+//   };
+// }
+
+export default combineReducers({
+  movies,
+  search,
+});
