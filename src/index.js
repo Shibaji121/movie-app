f5ba6bcc8bcfcc4fa0a3673a859a0d91201c97a4
shiba_1234 <shibajisahu121@gmail.com>
2023-03-15 00:13:28 +0530

    Search and add to list is implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
 // Currying function
 // logger(obj)(next)(action)
@@ -21,11 +22,24 @@ const logger =
   ({ dispatch, getState }) =>
   (next) =>
   (action) => {
-    console.log("ACTION_TYPE = ", action.type);
+    if (typeof action !== "function") {
+      console.log("ACTION_TYPE = ", action.type);
+    }
     next(action);
   };
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const thunk =
+//   ({ dispatch, getState }) =>
+//   (next) =>
+//   (action) => {
+//     if (typeof action === "function") {
+//       action(dispatch);
+//       return;
+//     }
+//     next(action);
+//   };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 // store.dispatch({
 //   type: "ADD_MOVIES",
